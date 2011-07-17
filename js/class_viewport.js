a07e520b7334d7f90e5a5ee2e11deaa062e59a6e
Oz <dreadpirate@lifewanted.com>
2011-07-17 04:36:30 -0700

    Added attach shader method

--- js/class_viewport.js
+++ js/class_viewport.js
@@ -20,19 +20,44 @@ var Viewport = (function(){
         // Set our default state
         // TODO: Make the background color customizable
         var cx = this._context;
-        cx.clearColor( 0, 0, 0, 1 ); // black
+        cx.clearColor( 0.34, 0.32, 0.31, 1 ); // Dark tan
         cx.clearDepth( 1 );          // Clear everything
         cx.enable( cx.DEPTH_TEST );  // No Z fighting please
         cx.depthFunc( cx.LEQUAL );
         this.clear();
+
+        // Get and attach a default shader
+        this.attachShader( Shader.getDefault() );
     }
-    
+
+    // Clears the viewport.
     v.prototype.clear = function(){
         var cx = this._context;
         cx.clear( cx.COLOR_BUFFER_BIT | cx.DEPTH_BUFFER_BIT );
     };
 
-    
+    // Attaches the given shader to the viewport
+    v.prototype.attachShader = function( shaders ){
+        var cx = this._context;
+        var s  = cx.createProgram();
+        
+        // TODO: Remove old shaders here
+        
+        // Attach each of the shaders we're passed and then link.
+        $.each( shaders, function( type, shader ){
+            cx.attachShader( s, shader );
+        });
+        cx.linkProgram( s );
+
+        // Did the link work?
+        if( !cx.getProgramParameter( s, cx.LINK_STATUS ) )
+            throw new Error( 'Failed to link shader' );
+
+        // Link worked, lets use it
+        cx.useProgram( s );
+        var vertPosAttr = cx.getAttribLocation( s, 'aVertexPosition' );
+        cx.enableVertexAttribArray( vertPosAttr );
+    };
 
 return v;
 })(); // end Viewport
