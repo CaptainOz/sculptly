59362cb3de0e7fff3a843b9f46379dedd9efe617
Oz <dreadpirate@lifewanted.com>
2011-07-17 04:55:09 -0700

    Added basic shader class.

---
+++ js/class_shader.js
@@ -0,0 +1,21 @@
+
+// TODO: Document Shader class
+var Shader = (function(){
+    function s( args )
+    {
+        $.extend( this, args );
+    }
+    
+    // Fetches the default shaders.
+    s.getDefault = function(callback){
+        api.call( 'GetShader', {
+                name    : 'Default',
+                success : function(data){
+                    callback( new s( data ) );
+                }
+            });
+    };
+
+return s;
+})(); // end Shader class
+
