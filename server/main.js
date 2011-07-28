4e6ec8853c510301686bcb711bea0e7a97fa7f50
Oz <dreadpirate@lifewanted.com>
2011-07-28 06:03:28 +0000

    Node server now does basic file lookup and passes API calls off to the API module

--- server/main.js
+++ server/main.js
@@ -1,10 +1,10 @@
 
-console.log( '*** Starting Server ***' );
+try{
 
-var http = require('http');
-http.createServer(function(req,res){
-    res.writeHead( 200, {'Content-Type':'text/plain'} );
-    res.end( 'Hi\n' );
-}).listen( 80, '127.0.0.1' );
-
-console.log( '*** Server Started ***' );
+    var web = require('sculptlyWeb');
+    web.start();
+}
+catch( e ){
+    console.log( '*** Server FAILED ***' );
+    console.log( e );
+}
